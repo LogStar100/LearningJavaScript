474b43f2e2709bf782d6dba2d2201c3da44eb5cf
LogStar100 <logstar100@gmail.com>
2015-08-19 12:53:51 -0400

    Create challengeOne.js

---
+++ challengeOne.js
@@ -0,0 +1,7 @@
+// My first JavaScript file! WOO!
+
+function challengeOne (name, age, userName) {
+    alert("Your name is" + " " + name + ", you are" + " " + age + " " + "years old," + " "     + "and your Reddit username is" + " " + userName + ".")
+}
+
+challengeOne(prompt("What is your name?"), prompt("How old are you?"), prompt("What is your Reddit username?"))
